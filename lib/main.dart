c396d2f1c1360f84178916c94fa5b17edacb21ba
Iandi Santulus <iandisantulusn@gmail.com>
2021-09-23 17:52:19 +0700

    `🚧refactor`: remove template + add HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:collection';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_isolated_worker_fetch_example/core/http_service.dart';
+
+import 'models/user.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +13,178 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'IsolatedWorker Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  static const String _targetUrl = 'https://jsonplaceholder.typicode.com/users';
+  final HttpService _httpService = HttpService();
 
-  void _incrementCounter() {
+  List<User>? _users;
+  Object? _error;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _refresh();
+  }
+
+  Future<void> _refresh() async {
+    if (_isLoading) return;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isLoading = true;
     });
+    _users = null;
+    _error = null;
+    try {
+      final List<User> fetchedUsers = await _fetchUsers();
+      setState(() {
+        _isLoading = false;
+        _users = fetchedUsers;
+      });
+    } catch (error) {
+      setState(() {
+        _isLoading = false;
+        _error = error;
+      });
+    }
+  }
+
+  Future<List<User>> _fetchUsers() async {
+    final LinkedHashMap<dynamic, dynamic> responseMap = await _httpService.get(_targetUrl);
+    final Object? error = responseMap['err'];
+    if (error != null) {
+      throw error;
+    }
+    final List<dynamic> jsonResponse = responseMap['jsonResponse'];
+    final List<User> users = jsonResponse.map<User>((dynamic raw) {
+      return User.fromMap(raw);
+    }).toList();
+    return users;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('IsolatedWorker fetch example'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: RefreshIndicator(
+        onRefresh: _refresh,
+        child: _currentState,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget get _currentState {
+    if (_isLoading) {
+      return const LoadingIndicator();
+    }
+    if (_error != null) {
+      return ErrorText(
+        error: _error!,
+        onRefresh: _refresh,
+      );
+    }
+    return UserList(users: _users!);
+  }
+}
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: CupertinoActivityIndicator());
+  }
+}
+
+class ErrorText extends StatelessWidget {
+  const ErrorText({
+    Key? key,
+    required this.error,
+    required this.onRefresh,
+  }) : super(key: key);
+
+  final Object error;
+  final VoidCallback onRefresh;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(error.toString()),
+          TextButton(
+            onPressed: onRefresh,
+            child: const Text('Reload'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class UserList extends StatelessWidget {
+  const UserList({
+    Key? key,
+    required this.users,
+  }) : super(key: key);
+
+  final List<User> users;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(20),
+      itemBuilder: (_, index) {
+        return UserListTile(user: users[index]);
+      },
+      itemCount: users.length,
+    );
+  }
+}
+
+class UserListTile extends StatelessWidget {
+  const UserListTile({
+    Key? key,
+    required this.user,
+  }) : super(key: key);
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Divider(),
+        Text(user.name),
+        Text(user.username),
+        Text(user.phone),
+        Text(user.email),
+        Text(user.website),
+        const Divider(),
+      ],
     );
   }
 }
